de8dee34b33a71ef738244e7d6dd409e27c47e12
sharathkumar-tm <sharath.kumar@trainingmug.com>
2024-11-11 04:06:35 +0530

    private access modifier

---
+++ src/com/trainginmug/emps/employee/Payroll.java
@@ -0,0 +1,21 @@
+package com.trainginmug.emps.employee;
+
+public class Payroll {
+
+    int empId;
+    double salary;
+
+    Department department;
+
+    public Payroll() {
+       // department = new Department(); //can't access since it is private
+        department = new Department(111, "development", "Madhu Samala");
+    }
+
+    void processEmployeeSalary(){
+        System.out.println("Employee Salary is processed");
+        System.out.println("Department id : " + department.id);
+        //System.out.println("Department Name : " + department.name); can't access since it is a private property
+        department.departmentDetails();
+    }
+}
